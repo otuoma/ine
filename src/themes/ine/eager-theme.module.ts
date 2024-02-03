8145aed1a4b3b16cbaac69e102b450e9ee5f46e3
Otuoma <otuomasp>
2024-02-03 21:13:10 +0300

    added scrollTo feature for companyIcons

--- src/themes/ine/eager-theme.module.ts
+++ src/themes/ine/eager-theme.module.ts
@@ -58,6 +58,7 @@ import { LogInComponent } from './app/shared/log-in/log-in.component';
 import { BrowseEntryListElementComponent } from './app/shared/object-list/browse-entry-list-element/browse-entry-list-element.component';
 import { PersonComponent } from './app/entity-groups/research-entities/item-pages/person/person.component';
 import { LangSwitchComponent } from './app/shared/lang-switch/lang-switch.component';
+import {ScrollToModule} from '@nicky-lenaers/ngx-scroll-to';
 
 
 /**
@@ -112,6 +113,7 @@ const DECLARATIONS = [
     ItemPageModule,
     ItemSharedModule,
     DsoPageModule,
+    ScrollToModule,
   ],
   declarations: DECLARATIONS,
   providers: [
